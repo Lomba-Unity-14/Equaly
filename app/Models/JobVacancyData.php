e46126215839fca80794c78a917b100d35005b05
brianabrar <briant.abbrar@gmail.com>
2026-05-06 15:17:05 +0700

    feat: migration lowongan kerja

---
+++ app/Models/JobVacancyData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable([
+    'url',
+    'company',
+    'location',
+    'job_title',
+    'jobdesk',
+    'salary',
+    'job_category',
+    'skill_req',
+    'education_req',
+    'work_type',
+    'company_size',
+    'image_logo_url',
+])]
+class JobVacancyData extends Model
+{
+    //
+}
